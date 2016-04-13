e3cfb9a286d40370b90b40272858fd4c0118a632
sdickreuter <Simon.Dickreuter@uni-tuebingen.de>
2016-04-13 16:04:43 +0200

    added more function to camera

--- Andor/camera.pyx
+++ Andor/camera.pyx
@@ -4,11 +4,15 @@ import time
 from Andor.errorcodes import ERROR_CODE
 cimport atmcdLXd as lib
 
+import numpy as np
+cimport numpy as np
+
 class Camera:
 
     verbosity = 2
-    init_path = '/usr/local/etc/andor'
-
+    _init_path = '/usr/local/etc/andor'
+    _width = 0
+    _height = 0
 
     def __init__(self):
         print("camera")
@@ -21,13 +25,43 @@ class Camera:
                 print("[%s]: %s" % (function, ERROR_CODE[error]))
 
     def Initialize(self):
+        self._Initialize()
+
+        #//Set Read Mode to --Image--
+        self._SetReadMode(4);
+
+        #//Set Acquisition mode to --Single scan--
+        self._SetAcquisitionMode(1);
+
+        #//Set initial exposure time
+        self._SetExposureTime(0.1);
+
+        #//Get Detector dimensions
+        self._width, self._heigt = self._GetDetector()
+
+        #//Initialize Shutter
+        self._SetShutter(1,0,50,50);
+
+        #//Setup Image dimensions
+        self._SetImage(1,1,1,self._width,1,self._height);
+
+    def TakeImage(self):
+        self._StartAcquisition()
+
+        while self._GetStatus() is 20072:
+            time.sleep(0.000001)
+
+        data = self._GetAcquiredData(self._width,self._height)
+        print(data.shape)
+
+    def _Initialize(self):
         dir_bytes = self.init_path.encode('UTF-8')
         cdef char* dir = dir_bytes
         error = lib.Initialize(dir)
         time.sleep(0.2)
         self.verbose(error, sys._getframe().f_code.co_name)
 
-    def GetDetector(self):
+    def _GetDetector(self):
         cdef int width = 0
         cdef int* width_ptr = &width
         cdef int height = 0
@@ -36,27 +70,47 @@ class Camera:
         self.verbose(error, sys._getframe().f_code.co_name)
         return width, height
 
-    def SetAcquisitionMode(self, mode):
+    def _SetAcquisitionMode(self, mode):
         error = lib.SetAcquisitionMode(mode)
         self.verbose(error, sys._getframe().f_code.co_name)
 
-    def SetReadMode(self, mode):
+    def _SetReadMode(self, mode):
         error = lib.SetReadMode(mode)
         self.verbose(error, sys._getframe().f_code.co_name)
 
-    def SetExposureTime(self,seconds):
+    def _SetExposureTime(self,seconds):
         error = lib.SetExposureTime(seconds)
         self.verbose(error, sys._getframe().f_code.co_name)
 
-    def SetShutter(self, typ, mode, closingtime, openingtime):
+    def _SetImage(self, hbin, vbin, hstart, hend, vstart, vend):
+        error = lib.SetImage(hbin, vbin, hstart, hend, vstart, vend)
+        self.verbose(error, sys._getframe().f_code.co_name)
+
+    def _SetShutter(self, typ, mode, closingtime, openingtime):
         error = lib.SetShutter(typ, mode, closingtime, openingtime)
         self.verbose(error, sys._getframe().f_code.co_name)
 
+    def _StartAcquisition(self):
+        error = lib.StartAcquistion()
+        self.verbose(error, sys._getframe().f_code.co_name)
 
-    def GetNumberDevices(self):
+    def _GetNumberDevices(self):
         cdef int num = -1
         cdef int* num_ptr = &num
         error = lib.GetNumberDevices(num_ptr)
         self.verbose(error, sys._getframe().f_code.co_name)
         print(num)
 
+    def _GetStatus(self):
+        cdef int status = 0
+        cdef int* status_ptr = &status
+        error = lib.GetStatus(status_ptr)
+        self.verbose(error, sys._getframe().f_code.co_name)
+        return status
+
+    def _GetAcquiredData(width,height,self):
+        cdef int size = width*height
+        cdef np.ndarray[int,mode="c"] data = np.zeros(size,dtype=int)
+        cdef int* data_ptr = &data[0]
+        error = lib.GetAcquiredData(data_ptr, size)
+        return data
