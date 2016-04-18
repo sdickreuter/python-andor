9aa9336903cf0f7a14e50b693f94643231428e57
sdickreuter <Simon.Dickreuter@uni-tuebingen.de>
2016-04-18 11:39:17 +0200

    trying to fix GetAcquiredData

--- Andor/camera.pyx
+++ Andor/camera.pyx
@@ -131,9 +131,9 @@ class Camera:
 
     def _GetAcquiredData(width,height,self):
         cdef unsigned int size = width*height
-        cdef np.ndarray[np.int, ndim=1, mode = 'c'] data = np.ascontiguousarray(np.zeros(size), dtype = np.int)
-        cdef int* data_ptr = <int*> data.data
-        #cdef np.ndarray[int,mode="c"] data = np.zeros(size,dtype=int)
-        #cdef int* data_ptr = &data[0]
+        #cdef np.ndarray[np.int, ndim=1, mode = 'c'] data = np.ascontiguousarray(np.zeros(size), dtype = np.int)
+        #cdef int* data_ptr = <int*> data.data
+        cdef np.ndarray[np.int,mode="c"] data = np.zeros(size,dtype=np.int)
+        cdef int* data_ptr = &data.data[0]
         error = lib.GetAcquiredData(data_ptr, size)
         return data
