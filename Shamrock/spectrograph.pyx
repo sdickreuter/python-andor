087e60b7988522635ea08e586d747645544e5ddb
sdickreuter <Simon.Dickreuter@uni-tuebingen.de>
2016-04-20 16:26:41 +0200

    different fixes

--- Shamrock/spectrograph.pyx
+++ Shamrock/spectrograph.pyx
@@ -120,8 +120,7 @@ class Spectrograph:
         #cdef float *waves = &waves_numpy[0]
         #error = lib.ShamrockGetCalibration(self.device, waves, n)
         #waves_numpy = np.array(waves)
-        cdef np.float32[:] waves = np.zeros(numberpixels, dtype=np.float32)
-        waves = input.view(np.float32)
+        cdef float[:] waves = np.zeros(numberpixels, dtype=np.float32)
         error = lib.ShamrockGetCalibration(self.device, <float *>&waves, n)
         #print(waves_numpy[0])
         self.verbose(error, "_GetCalibration")
