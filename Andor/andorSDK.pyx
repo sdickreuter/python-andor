1cb88885b44c1a8f43385c2dde70fa9418136d56
sdickreuter <Simon.Dickreuter@uni-tuebingen.de>
2016-07-15 14:59:26 +0200

    wrapped everything in classes

--- Andor/andorSDK.pyx
+++ Andor/andorSDK.pyx
@@ -10,178 +10,181 @@ from cpython cimport array
 import array
 import time
 
-verbosity = 2
-_init_path = '/usr/local/etc/andor'
 
-AcquistionModes = { 'Single Scan' : 1,
-                    'Accumulate' : 2,
-                    'Kinetics' : 3,
-                    'Fast Kinetics' : 4,
-                    'Run till abort' : 5}
+cdef class Andor:
 
-def verbose(error, function=''):
-    if verbosity > 0:
-        if not error == 20002:
-            print("[%s]: %s" % (function, ERROR_CODE[error]))
-        elif verbosity > 1:
-            print("[%s]: %s" % (function, ERROR_CODE[error]))
+    verbosity = 2
+    _init_path = '/usr/local/etc/andor'
 
-def Shutdown():
-    lib.AbortAcquisition()
-    SetTemperature(-20)
-    if GetTemperature() <= -20:
-        print("Detector warming up, please wait.")
-        warm = False
-        while not warm:
-            time.sleep(1)
-            if GetTemperature() > -20:
-                warm = True
-        print("Warmup finished.")
+    AcquistionModes = { 'Single Scan' : 1,
+                        'Accumulate' : 2,
+                        'Kinetics' : 3,
+                        'Fast Kinetics' : 4,
+                        'Run till abort' : 5}
 
-    CoolerOFF()
-    error = lib.ShutDown()
-    #verbose(error, sys._getframe().f_code.co_name)
+    def verbose(self, error, function=''):
+        if self.verbosity > 0:
+            if not error == 20002:
+                print("[%s]: %s" % (function, ERROR_CODE[error]))
+            elif self.verbosity > 1:
+                print("[%s]: %s" % (function, ERROR_CODE[error]))
 
-def Initialize():
-    dir_bytes = _init_path.encode('UTF-8')
-    cdef char* dir = dir_bytes
-    error = lib.Initialize(dir)
-    time.sleep(2)
-    verbose(error, "Initialize")
-    if error is 20002:
-        return 1
-    else:
-        return 0
+    def Shutdown(self):
+        lib.AbortAcquisition()
+        self.SetTemperature(-20)
+        if self.GetTemperature() <= -20:
+            print("Detector warming up, please wait.")
+            warm = False
+            while not warm:
+                time.sleep(1)
+                if self.GetTemperature() > -20:
+                    warm = True
+            print("Warmup finished.")
 
-def AbortAcquisition():
-    error = lib.AbortAcquisition()
-    verbose(error, "AbortAcquisition")
+        self.CoolerOFF(self)
+        error = lib.ShutDown()
+        #verbose(error, sys._getframe().f_code.co_name)
 
-def GetDetector():
-    cdef int width = 0
-    cdef int* width_ptr = &width
-    cdef int height = 0
-    cdef int* height_ptr = &height
-    error = lib.GetDetector(width_ptr,height_ptr)
-    verbose(error, "GetDetector")
-    return width, height
+    def Initialize(self):
+        dir_bytes = self._init_path.encode('UTF-8')
+        cdef char* dir = dir_bytes
+        error = lib.Initialize(dir)
+        time.sleep(2)
+        self.verbose(error, "Initialize")
+        if error is 20002:
+            return 1
+        else:
+            return 0
 
-def SetAcquisitionMode(mode):
-    cdef int m = mode
-    error = lib.SetAcquisitionMode(m)
-    verbose(error, "SetAcquisitionMode")
+    def AbortAcquisition(self):
+        error = lib.AbortAcquisition()
+        self.verbose(error, "AbortAcquisition")
 
-def SetReadMode(mode):
-    cdef int m = mode
-    error = lib.SetReadMode(m)
-    verbose(error, "SetReadMode")
+    def GetDetector(self):
+        cdef int width = 0
+        cdef int* width_ptr = &width
+        cdef int height = 0
+        cdef int* height_ptr = &height
+        error = lib.GetDetector(width_ptr,height_ptr)
+        self.verbose(error, "GetDetector")
+        return width, height
 
-def SetExposureTime(float seconds):
-    error = lib.SetExposureTime(seconds)
-    verbose(error, "SetExposureTime")
-    print(error)
-    print(GetAcquisitionTimings())
+    def SetAcquisitionMode(self, mode):
+        cdef int m = mode
+        error = lib.SetAcquisitionMode(m)
+        self.verbose(error, "SetAcquisitionMode")
 
-def GetAcquisitionTimings():
-    cdef float exposure = 0
-    cdef float* exposure_ptr = &exposure
-    cdef float accumulate = 0
-    cdef float* accumulate_ptr = &accumulate
-    cdef float kinetic = 0
-    cdef float* kinetic_ptr = &kinetic
-    error = lib.GetAcquisitionTimings(exposure_ptr, accumulate_ptr, kinetic_ptr)
-    verbose(error, "GetAcquisitionTimings")
-    return exposure
+    def SetReadMode(self, mode):
+        cdef int m = mode
+        error = lib.SetReadMode(m)
+        self.verbose(error, "SetReadMode")
 
-def SetImage(int hbin,int vbin,int hstart,int hend,int vstart,int vend):
-    error = lib.SetImage(hbin, vbin, hstart, hend, vstart, vend)
-    verbose(error, "SetImage")
+    def SetExposureTime(self, float seconds):
+        error = lib.SetExposureTime(seconds)
+        self.verbose(error, "SetExposureTime")
+        print(error)
+        print(self.GetAcquisitionTimings())
 
-def SetShutter(int typ,int mode,int closingtime,int openingtime):
-    error = lib.SetShutter(typ, mode, closingtime, openingtime)
-    verbose(error, "SetShutter")
+    def GetAcquisitionTimings(self):
+        cdef float exposure = 0
+        cdef float* exposure_ptr = &exposure
+        cdef float accumulate = 0
+        cdef float* accumulate_ptr = &accumulate
+        cdef float kinetic = 0
+        cdef float* kinetic_ptr = &kinetic
+        error = lib.GetAcquisitionTimings(exposure_ptr, accumulate_ptr, kinetic_ptr)
+        self.verbose(error, "GetAcquisitionTimings")
+        return exposure
 
-def StartAcquisition():
-    #status = GetStatus()
-    #if status == 20073: # 20073: 'DRV_IDLE'
-    #    error = lib.StartAcquisition()
-    #    verbose(error, "StartAcquisition")
-    #else:
-    #    print("[StartAcquisition]: not idle !")
-    #    verbose(20992, "StartAcquisition")
-    error = lib.StartAcquisition()
-    verbose(error, "StartAcquisition")
+    def SetImage(self, int hbin,int vbin,int hstart,int hend,int vstart,int vend):
+        error = lib.SetImage(hbin, vbin, hstart, hend, vstart, vend)
+        self.verbose(error, "SetImage")
+
+    def SetShutter(self, int typ,int mode,int closingtime,int openingtime):
+        error = lib.SetShutter(typ, mode, closingtime, openingtime)
+        self.verbose(error, "SetShutter")
+
+    def StartAcquisition(self):
+        #status = GetStatus()
+        #if status == 20073: # 20073: 'DRV_IDLE'
+        #    error = lib.StartAcquisition()
+        #    verbose(error, "StartAcquisition")
+        #else:
+        #    print("[StartAcquisition]: not idle !")
+        #    verbose(20992, "StartAcquisition")
+        error = lib.StartAcquisition()
+        self.verbose(error, "StartAcquisition")
 
 
-def GetNumberDevices():
-    cdef int num = -1
-    cdef int* num_ptr = &num
-    error = lib.GetNumberDevices(num_ptr)
-    verbose(error, "GetNumberDevices")
-    return num
+    def GetNumberDevices(self):
+        cdef int num = -1
+        cdef int* num_ptr = &num
+        error = lib.GetNumberDevices(num_ptr)
+        self.verbose(error, "GetNumberDevices")
+        return num
 
-def GetStatus():
-    cdef int status = 0
-    cdef int* status_ptr = &status
-    error = lib.GetStatus(status_ptr)
-    #verbose(error, "_GetStatus")
-    return status
+    def GetStatus(self):
+        cdef int status = 0
+        cdef int* status_ptr = &status
+        error = lib.GetStatus(status_ptr)
+        #verbose(error, "_GetStatus")
+        return status
 
-def GetAcquiredData(width, height):
-    cdef unsigned int size = width*height
-    cdef array.array data = array.array('i', np.zeros(size,dtype=np.int))
-    error = lib.GetAcquiredData(data.data.as_ints, size)
-    verbose(error, "GetAcquiredData")
-    return np.array(data).reshape((height,width)).transpose()
+    def GetAcquiredData(self, width, height):
+        cdef unsigned int size = width*height
+        cdef array.array data = array.array('i', np.zeros(size,dtype=np.int))
+        error = lib.GetAcquiredData(data.data.as_ints, size)
+        self.verbose(error, "GetAcquiredData")
+        return np.array(data).reshape((height,width)).transpose()
 
-def GetPixelSize():
-    cdef float xSize = 0
-    cdef float* xSize_ptr = &xSize
-    cdef float ySize = 0
-    cdef float* ySize_ptr = &ySize
-    error = lib.GetPixelSize(xSize_ptr, ySize_ptr)
-    verbose(error, "GetPixelSize")
-    return xSize, ySize
+    def GetPixelSize(self):
+        cdef float xSize = 0
+        cdef float* xSize_ptr = &xSize
+        cdef float ySize = 0
+        cdef float* ySize_ptr = &ySize
+        error = lib.GetPixelSize(xSize_ptr, ySize_ptr)
+        self.verbose(error, "GetPixelSize")
+        return xSize, ySize
 
-def GetTECStatus():
-    cdef int flag = 0
-    cdef int* flag_ptr = &flag
-    error = lib.GetTECStatus(flag_ptr)
-    if (flag):
-        print("ERROR: TEC has overheated !")
-    verbose(error, "GetTECStatus")
+    def GetTECStatus(self):
+        cdef int flag = 0
+        cdef int* flag_ptr = &flag
+        error = lib.GetTECStatus(flag_ptr)
+        if (flag):
+            print("ERROR: TEC has overheated !")
+        self.verbose(error, "GetTECStatus")
 
-def GetTemperature():
-    cdef int temp = 0
-    cdef int* temp_ptr = &temp
-    error = lib.GetTemperature(temp_ptr)
-    if (error != 20035) and (error != 20037):
-        verbose(error, "GetTemperature")
-    return temp
+    def GetTemperature(self):
+        cdef int temp = 0
+        cdef int* temp_ptr = &temp
+        error = lib.GetTemperature(temp_ptr)
+        if (error != 20035) and (error != 20037):
+            self.verbose(error, "GetTemperature")
+        return temp
 
-def GetTemperatureRange():
-    cdef int min_temp = 0
-    cdef int* min_temp_ptr = &min_temp
-    cdef int max_temp = 0
-    cdef int* max_temp_ptr = &max_temp
-    error = lib.GetTemperatureRange(min_temp_ptr, max_temp_ptr)
-    verbose(error, "GetTemperatureRange")
-    return min_temp, max_temp
+    def GetTemperatureRange(self):
+        cdef int min_temp = 0
+        cdef int* min_temp_ptr = &min_temp
+        cdef int max_temp = 0
+        cdef int* max_temp_ptr = &max_temp
+        error = lib.GetTemperatureRange(min_temp_ptr, max_temp_ptr)
+        self.verbose(error, "GetTemperatureRange")
+        return min_temp, max_temp
 
-def SetTemperature(temperature):
-    cdef int temp = temperature
-    error = lib.SetTemperature(temp)
-    verbose(error, "SetTemperatur")
+    def SetTemperature(self, temperature):
+        cdef int temp = temperature
+        error = lib.SetTemperature(temp)
+        self.verbose(error, "SetTemperatur")
 
-def CoolerON():
-    error = lib.CoolerON()
-    verbose(error, "CoolerON")
+    def CoolerON(self):
+        error = lib.CoolerON()
+        self.verbose(error, "CoolerON")
 
-def CoolerOFF():
-    error = lib.CoolerOFF()
-    verbose(error, "CoolerOFF")
+    def CoolerOFF(self):
+        error = lib.CoolerOFF()
+        self.verbose(error, "CoolerOFF")
 
-def SetNumberAccumulations(number):
-    cdef int n = number
-    error = lib.SetNumberAccumulations(n)
-    verbose(error, "SetNumberAccumulations")
+    def SetNumberAccumulations(self, number):
+        cdef int n = number
+        error = lib.SetNumberAccumulations(n)
+        self.verbose(error, "SetNumberAccumulations")
--- Shamrock/shamrockSDK.pyx
+++ Shamrock/shamrockSDK.pyx
@@ -9,146 +9,140 @@ cimport numpy as np
 from cpython cimport array
 import array
 
-verbosity = 2
-init_path = '/usr/local/etc/andor'
-device = 0
 
-def verbose(error, function=''):
-    if verbosity > 0:
-        if not error == 20202:
-            print("[%s]: %s" % (function, ERROR_CODE[error]))
-        elif verbosity > 1:
-            print("[%s]: %s" % (function, ERROR_CODE[error]))
+cdef class Shamrock:
 
-def Initialize():
-    dir_bytes = init_path.encode('UTF-8')
-    cdef char* dir = dir_bytes
-    error = lib.ShamrockInitialize(dir)
-    verbose(error, "Initialize")
-    if error is 20202:
-        return 1
-    else:
-        return 0
-    
-def Shutdown():
-    lib.ShamrockClose()
+    verbosity = 2
+    init_path = '/usr/local/etc/andor'
+    device = 0
 
-def GetNumberDevices():
-    cdef int num = -1
-    cdef int* num_ptr = &num
-    error = lib.ShamrockGetNumberDevices(num_ptr)
-    verbose(error, "GetNumberDevices")
-    print(num)
+    def verbose(self, error, function=''):
+        if self.verbosity > 0:
+            if not error == 20202:
+                print("[%s]: %s" % (function, ERROR_CODE[error]))
+            elif self.verbosity > 1:
+                print("[%s]: %s" % (function, ERROR_CODE[error]))
 
-def SetPixelWidth(width):
-    cdef float w = width
-    error = lib.ShamrockSetPixelWidth(device, w)
-    verbose(error, "SetPixelWidth")
+    def Initialize(self):
+        dir_bytes = self.init_path.encode('UTF-8')
+        cdef char* dir = dir_bytes
+        error = lib.ShamrockInitialize(dir)
+        self.verbose(error, "Initialize")
+        if error is 20202:
+            return 1
+        else:
+            return 0
 
-def SetNumberPixels(pixelnumber):
-    cdef int n = pixelnumber
-    error = lib.ShamrockSetNumberPixels(device, n)
-    verbose(error, "SetNumberPixels")
+    def Shutdown(self):
+        lib.ShamrockClose()
 
-def SetGrating(grating):
-    cdef int g = grating
-    error = lib.ShamrockSetGrating(device, g)
-    verbose(error, "SetGrating")
+    def GetNumberDevices(self):
+        cdef int num = -1
+        cdef int* num_ptr = &num
+        error = lib.ShamrockGetNumberDevices(num_ptr)
+        self.verbose(error, "GetNumberDevices")
+        print(num)
 
-def GetGrating():
-    cdef int grating = 0
-    cdef int* grating_ptr = &grating
-    error = lib.ShamrockGetGrating(device, grating_ptr)
-    verbose(error, "GetGrating")
-    return grating
+    def SetPixelWidth(self, width):
+        cdef float w = width
+        error = lib.ShamrockSetPixelWidth(self.device, w)
+        self.verbose(error, "SetPixelWidth")
 
-def GetNumberGratings():
-    cdef int n = 0
-    cdef int* n_ptr = &n
-    error = lib.ShamrockGetNumberGratings(device, n_ptr)
-    verbose(error, "GetNumberGratings")
-    return n
+    def SetNumberPixels(self, pixelnumber):
+        cdef int n = pixelnumber
+        error = lib.ShamrockSetNumberPixels(self.device, n)
+        self.verbose(error, "SetNumberPixels")
 
-def GetGratingInfo(grating):
-    #unsigned int ShamrockGetGratingInfo(int device,int Grating, float *Lines,  char* Blaze, int *Home, int *Offset)
-    cdef int g = grating
-    cdef float lines = 0
-    cdef float* lines_ptr = &lines
-    blaze_bytes = '     '.encode('UTF-8') # 5 chars long !
-    cdef char* blaze = blaze_bytes
-    cdef int home = 0
-    cdef int* home_ptr = &home
-    cdef int offset = 0
-    cdef int* offset_ptr = &offset
-    error = lib.ShamrockGetGratingInfo(device, g, lines_ptr, blaze, home_ptr, offset_ptr)
-    verbose(error, "GetGratingInfo")
-    return lines, blaze, home, offset
+    def SetGrating(self, grating):
+        cdef int g = grating
+        error = lib.ShamrockSetGrating(self.device, g)
+        self.verbose(error, "SetGrating")
 
-def SetWavelength(wavelength):
-    cdef float wl = wavelength
-    error = lib.ShamrockSetWavelength(device, wl)
-    verbose(error, "SetWavelength")
+    def GetGrating(self):
+        cdef int grating = 0
+        cdef int* grating_ptr = &grating
+        error = lib.ShamrockGetGrating(self.device, grating_ptr)
+        self.verbose(error, "GetGrating")
+        return grating
 
-def GetWavelength():
-    cdef float wl = 0
-    cdef float* wl_ptr = &wl
-    error = lib.ShamrockGetWavelength(device, wl_ptr)
-    verbose(error, "GetWavelength")
-    return wl
+    def GetNumberGratings(self):
+        cdef int n = 0
+        cdef int* n_ptr = &n
+        error = lib.ShamrockGetNumberGratings(self.device, n_ptr)
+        self.verbose(error, "GetNumberGratings")
+        return n
 
-def GetWavelengthLimits(grating):
-    cdef int g = grating
-    cdef float min = 0
-    cdef float* min_ptr = &min
-    cdef float max = 0
-    cdef float* max_ptr = &max
-    error = lib.ShamrockGetWavelengthLimits(device, g, min_ptr, max_ptr)
-    verbose(error, "GetWavelengthLimits")
-    return min, max
+    def GetGratingInfo(self, int grating):
+        #unsigned int ShamrockGetGratingInfo(int device,int Grating, float *Lines,  char* Blaze, int *Home, int *Offset)
+        cdef float lines = 0
+        cdef float* lines_ptr = &lines
+        blaze_bytes = '     '.encode('UTF-8') # 5 chars long !
+        cdef char* blaze = blaze_bytes
+        cdef int home = 0
+        cdef int* home_ptr = &home
+        cdef int offset = 0
+        cdef int* offset_ptr = &offset
+        error = lib.ShamrockGetGratingInfo(self.device, grating, lines_ptr, blaze, home_ptr, offset_ptr)
+        self.verbose(error, "GetGratingInfo")
+        return lines, blaze, home, offset
 
-def GetCalibration(numberpixels):
-    cdef int n = numberpixels
-    #cdef array.array values = array.array('f', np.zeros(numberpixels,dtype=np.float))
-    #cdef float  waves[n]
-    #cdef np.ndarray[float, ndim=1, mode="c"] waves_numpy = np.empty(numberpixels,dtype=np.float32)
-    #cdef float *waves = &waves_numpy[0]
-    #error = lib.ShamrockGetCalibration(device, waves, n)
-    #waves_numpy = np.array(waves)
-    #cdef float[:] waves = np.zeros(numberpixels, dtype=np.float32)
-    #error = lib.ShamrockGetCalibration(device, <float *>&waves, n)
-    cdef array.array waves = array.array('f', np.zeros(numberpixels,dtype=np.float32))
-    error = lib.ShamrockGetCalibration(device, waves.data.as_floats, numberpixels)
+    def SetWavelength(self, float wavelength):
+        error = lib.ShamrockSetWavelength(self.device, wavelength)
+        self.verbose(error, "SetWavelength")
 
-    #print(waves_numpy[0])
-    verbose(error, "GetCalibration")
-    return np.array(waves)
+    def GetWavelength(self):
+        cdef float wl = 0
+        cdef float* wl_ptr = &wl
+        error = lib.ShamrockGetWavelength(self.device, wl_ptr)
+        self.verbose(error, "GetWavelength")
+        return wl
 
-def SetFlipperMirrorPosition(flipper, position):
-    cdef int f = flipper
-    cdef int pos = position
-    error = lib.ShamrockSetFlipperMirrorPosition(device, f, pos)
-    verbose(error, "SetFlipperMirrorPosition")
+    def GetWavelengthLimits(self, grating):
+        cdef int g = grating
+        cdef float min = 0
+        cdef float* min_ptr = &min
+        cdef float max = 0
+        cdef float* max_ptr = &max
+        error = lib.ShamrockGetWavelengthLimits(self.device, g, min_ptr, max_ptr)
+        self.verbose(error, "GetWavelengthLimits")
+        return min, max
 
-def GetFlipperMirrorPosition(flipper):
-    cdef int f = flipper
-    cdef int pos = 0
-    cdef int* pos_ptr = &pos
-    error = lib.ShamrockGetFlipperMirrorPosition(device, f, pos_ptr)
-    verbose(error, "GetFlipperMirrorPosition")
-    return pos
+    def GetCalibration(self, int numberpixels):
+        #cdef array.array values = array.array('f', np.zeros(numberpixels,dtype=np.float))
+        #cdef float  waves[n]
+        #cdef np.ndarray[float, ndim=1, mode="c"] waves_numpy = np.empty(numberpixels,dtype=np.float32)
+        #cdef float *waves = &waves_numpy[0]
+        #error = lib.ShamrockGetCalibration(device, waves, n)
+        #waves_numpy = np.array(waves)
+        #cdef float[:] waves = np.zeros(numberpixels, dtype=np.float32)
+        #error = lib.ShamrockGetCalibration(device, <float *>&waves, n)
+        cdef array.array waves = array.array('f', np.zeros(numberpixels,dtype=np.float32))
+        error = lib.ShamrockGetCalibration(self.device, waves.data.as_floats, numberpixels)
 
-def SetAutoSlitWidth(index, width): # width in um !
-    cdef int i = index
-    cdef float w = width
-    error = lib.ShamrockSetAutoSlitWidth(device, i, w)
-    verbose(error, "SetAutoSlitWidth")
+        #print(waves_numpy[0])
+        self.verbose(error, "GetCalibration")
+        return np.array(waves)
 
-def GetAutoSlitWidth(index):
-    cdef int i = index
-    cdef float w = 0
-    cdef float* w_ptr = &w
-    error = lib.ShamrockGetAutoSlitWidth(device, i, w_ptr)
-    verbose(error, "GetAutoSlitWidth")
-    return w # w in um !
+    def SetFlipperMirrorPosition(self, int flipper, int position):
+        error = lib.ShamrockSetFlipperMirrorPosition(self.device, flipper, position)
+        self.verbose(error, "SetFlipperMirrorPosition")
+
+    def GetFlipperMirrorPosition(self, int flipper):
+        cdef int pos = 0
+        cdef int* pos_ptr = &pos
+        error = lib.ShamrockGetFlipperMirrorPosition(self.device, flipper, pos_ptr)
+        self.verbose(error, "GetFlipperMirrorPosition")
+        return pos
+
+    def SetAutoSlitWidth(self, int index, float width): # width in um !
+        error = lib.ShamrockSetAutoSlitWidth(self.device, index, width)
+        self.verbose(error, "SetAutoSlitWidth")
+
+    def GetAutoSlitWidth(self, int index):
+        cdef float w = 0
+        cdef float* w_ptr = &w
+        error = lib.ShamrockGetAutoSlitWidth(self.device, index, w_ptr)
+        self.verbose(error, "GetAutoSlitWidth")
+        return w # w in um !
 
 
